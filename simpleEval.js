6d9d198a761af9f67f4a79d2c2ace2e4559b2078
Cathewrine Brower <catherinemb42@gmail.com>
2022-12-12 13:38:10 -0500

    adding functions

--- simpleEval.js
+++ simpleEval.js
@@ -1,6 +1,6 @@
 const ASSOC = {
-    left: 0,
-    right: 1
+    Left: 0,
+    Right: 1
 }
 
 const OPERATORS = {
@@ -41,6 +41,17 @@ const OPERATORS = {
     }
 }
 
+const FUNCTIONS = {
+    "sin": {
+        precedence: 10,
+        associativity: ASSOC.Right,
+        num_operands: 1,
+        eval: (args) => {
+            return Math.sin(parseFloat(args[0]));
+        }
+    }
+}
+
 function evaluate(postfixStack) {
     if(postfixStack.length < 1) {
         return null;
@@ -48,10 +59,18 @@ function evaluate(postfixStack) {
 
     let tempStack = [];
     while(postfixStack.length > 0) {
-        if(isOperator(postfixStack[postfixStack.length - 1])) {
-            tempStack.push(OPERATORS[postfixStack.pop()].eval(parseFloat(tempStack.pop()), parseFloat(tempStack.pop())))
+        let token = postfixStack.pop();
+
+        if(isOperator(token)) {
+            tempStack.push(OPERATORS[token].eval(parseFloat(tempStack.pop()), parseFloat(tempStack.pop())))
+        } else if(isFunction(token)) {
+            let args = [];
+            for(let i = 0; i < FUNCTIONS[token].num_operands; i++) {
+                args[i] = tempStack.pop();
+            }
+            tempStack.push(FUNCTIONS[token].eval(args));
         } else {
-            tempStack.push(postfixStack.pop())
+            tempStack.push(token)
         }
     }
 
@@ -67,12 +86,40 @@ function reverse(array) {
     return result;
 }
 
+function isFunction(string) {
+    return string in FUNCTIONS;
+}
+
 function isOperator(string) {
     return string in OPERATORS;
 }
 
 function isNumber(string) {
-    return string.match(/^[0-9]*$/)
+    return string.match(/^[0-9]+|\.?[0-9]+$/)
+}
+
+function getAssociativity(token) {
+    if(token in OPERATORS) {
+        return OPERATORS[token].associativity;
+    } else if (token in FUNCTIONS) {
+        return FUNCTIONS[token].associativity
+    } else {
+        throw `token ${token} undefined`
+    }
+}
+
+function getPrecedence(token) {
+    if(token in OPERATORS) {
+        return OPERATORS[token].precedence;
+    } else if (token in FUNCTIONS) {
+        return FUNCTIONS[token].precedence;
+    } else {
+        throw `token ${token} undefined`
+    }
+}
+
+function print(str) {
+    console.log(str);
 }
 
 function infixToPostfix(rawInfix) {
@@ -83,22 +130,28 @@ function infixToPostfix(rawInfix) {
         }
     });
 
+    // print(infix);
+
     let outputQueue = [];
     let operatorsStack = [];
     let token;
     for(let i = 0; i < infix.length; i++) {
+        // print(outputQueue);
         token = infix[i];
 
         if(isNumber(token)) {
-            outputQueue.push(token);
+            outputQueue.push(parseFloat(token));
         } else if(isOperator(token)) {
             let o1 = token;
             let o2 = operatorsStack[operatorsStack.length - 1];
-            while(isOperator(o2) && ((OPERATORS[o1].associativity === ASSOC.Left && OPERATORS[o1].precedence <= OPERATORS[o2].precedence) || (OPERATORS[o1].associativity === ASSOC.Right && OPERATORS[o1].precedence < OPERATORS[o2].precedence))) {
+            while((isOperator(o2) || isFunction(o2)) && ((getAssociativity(o1) === ASSOC.Left && getPrecedence(o1) <= getPrecedence(o2)) 
+                || (getAssociativity(o1) === ASSOC.Right && getPrecedence(o1) < getPrecedence(o2)))) {
                 outputQueue.push(operatorsStack.pop());
                 o2 = operatorsStack[operatorsStack.length - 1];
             }
             operatorsStack.push(o1);
+        } else if(isFunction(token)) {
+            operatorsStack.push(token);
         } else if(token === "(") {
             operatorsStack.push(token);
         } else if(token === ")") {
@@ -106,6 +159,9 @@ function infixToPostfix(rawInfix) {
                 outputQueue.push(operatorsStack.pop());
             }
             operatorsStack.pop();
+        } else {
+            console.log(`Unknown token: ${token}`)
+            exit();
         }
     }
 
@@ -116,4 +172,5 @@ function infixToPostfix(rawInfix) {
     return reverse(outputQueue);
 }
 
-console.log(evaluate(infixToPostfix(process.argv[2])));
+postfix = infixToPostfix(process.argv[2])
+console.log(evaluate(postfix));
